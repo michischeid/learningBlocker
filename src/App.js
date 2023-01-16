d45514647ca1a7c9ad40451aa80bd517143d511c
TerryTate <matthias@zwiesele.de>
2023-01-16 17:19:36 +0100

    Changed folder structure for components, added navigation bar, installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import Timer from './Timer'
-import CustomNotification from './CustomNotification'
+import Timer from './components/Timer'
+import CustomNotification from './components/CustomNotification'
+import NavigationBar from './components/NavigationBar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
 
   render() {
     return (
-    <div>
+      <div>
       <header>
         <CustomNotification text="Hello World" show={false}></CustomNotification>
       </header>
+      <NavigationBar></NavigationBar>
       <Timer name="Focus Time"></Timer>
     </div>
     );
